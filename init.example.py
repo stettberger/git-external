ceec740b9e5785a58e5fd7915cde029a49af33f4
Gerion Entrup <gerion.entrup@flump.de>
2019-11-30 13:15:45 +0100

    git-external: convert to argparse
    Unfortunately, the __set_name__ mechanism is only supported from Python
    3.6 on.

--- init.example.py
+++ init.example.py
@@ -1,16 +1,10 @@
 class EchoExtension:
-    def __init__(self, commands):
-        self.all_commands = commands
-
-    def cmd_echo(self, *args):
+    def cmd_echo(self, args):
         print(args)
-        print(self.all_commands)
 
-    def commands(self):
-        """An extension class is found by having this attribute. It is
-           supposed to return a dict structured like:
+    def echo(self, subparser):
+        subparser.set_defaults(func=self.cmd_echo)
+        subparser.add_argument("-v", "--verbose", help="be verbose",
+                               default=False, action="store_true")
 
-           {ARG: (HELP, CMD)}
-
-        """
-        return {'echo': (' -- echo everything (from init.example.py)', self.cmd_echo)}
+    _commands = [('echo', 'Help for echo', echo)]
